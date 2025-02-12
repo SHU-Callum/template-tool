c23c4360b12cfb1eeedbca51abdabb1e83007b61
Callum Cheng <c3072423@hallam.shu.ac.uk>
2025-02-12 18:41:12 +0000

    installed tailwind and created base app template

--- front-end/README.md
+++ front-end/README.md
@@ -1,2 +1,40 @@
 # Front-End Setup
 - [Home guide](../README.md)
+
+## Setup
+- `cd template-tool-ui`
+- `yarn`
+- `yarn dev`
+
+## Stack
+
+| Technology   | Description                                                              | Version |
+|--------------|--------------------------------------------------------------------------|---------|
+| React        | JavaScript library for building user interfaces                          | ^18.2.0 |
+| TypeScript   | A typed superset of JavaScript                                           | ^5.2.2  |
+| Vite         | Fast build tool and development server for modern web projects           | ^5.1.6  |
+| Tailwind CSS | Utility-first CSS framework for rapid UI development                     | 3       |
+| Electron     | Framework for building cross-platform desktop apps with web technologies | ^30.0.1 |
+| Yarn         | Package manager for managing project dependencies                        | ^1.22.19|
+
+## Dependencies
+
+- **React**: ^18.2.0
+  - **@types/react**: ^18.2.64
+  - **react-dom**: ^18.2.0
+  - **@types/react-dom**: ^18.2.21
+- **TypeScript**: ^5.2.2
+  - **@typescript-eslint/eslint-plugin**: ^7.1.1
+  - **@typescript-eslint/parser**: ^7.1.1
+- **Vite**: ^5.1.6
+  - **@vitejs/plugin-react**: ^4.2.1
+  - **vite-plugin-electron**: ^0.28.6
+  - **vite-plugin-electron-renderer**: ^0.14.5
+- **Tailwind CSS**: 3
+  - **postcss**: ^8.5.2
+  - **autoprefixer**: ^10.4.20
+- **Electron**: ^30.0.1
+  - **electron-builder**: ^24.13.3
+- **ESLint**: ^8.57.0
+  - **eslint-plugin-react-hooks**: ^4.6.0
+  - **eslint-plugin-react-refresh**: ^0.4.5
--- front-end/template-tool-ui/src/App.tsx
+++ front-end/template-tool-ui/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/electron-vite.animate.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const profileClicked = () => {
+    alert('Profile Clicked')
+  }
+  const infoClicked = () => {
+    alert('Info Clicked')
+  }
 
   return (
-    <>
-    <h1>Template Tool</h1>
-      <div>
-        <a href="https://electron-vite.github.io" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='p-4 pt-2 w-full h-full flex flex-col'>
+      <div className='flex grow w-full'>
+        <div className='flex w-full justify-between'>
+          <h1 className='text-left'>Template Tool</h1>
+            <svg onClick={profileClicked} className="w-12 h-12 text-gray-800 dark:text-white mt-2 hover:text-gray-600 dark:hover:text-gray-400 cursor-pointer transition-transform transform hover:scale-110" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z" clip-rule="evenodd"/>
+            </svg>
+        </div>
       </div>
-      
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className='flex justify-between items-center w-full'>
+        <div className='flex items-center'>
+          <svg className="w-8 h-8 text-green-400 pr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path d="M12 7.205c4.418 0 8-1.165 8-2.602C20 3.165 16.418 2 12 2S4 3.165 4 4.603c0 1.437 3.582 2.602 8 2.602ZM12 22c4.963 0 8-1.686 8-2.603v-4.404c-.052.032-.112.06-.165.09a7.75 7.75 0 0 1-.745.387c-.193.088-.394.173-.6.253-.063.024-.124.05-.189.073a18.934 18.934 0 0 1-6.3.998c-2.135.027-4.26-.31-6.3-.998-.065-.024-.126-.05-.189-.073a10.143 10.143 0 0 1-.852-.373 7.75 7.75 0 0 1-.493-.267c-.053-.03-.113-.058-.165-.09v4.404C4 20.315 7.037 22 12 22Zm7.09-13.928a9.91 9.91 0 0 1-.6.253c-.063.025-.124.05-.189.074a18.935 18.935 0 0 1-6.3.998c-2.135.027-4.26-.31-6.3-.998-.065-.024-.126-.05-.189-.074a10.163 10.163 0 0 1-.852-.372 7.816 7.816 0 0 1-.493-.268c-.055-.03-.115-.058-.167-.09V12c0 .917 3.037 2.603 8 2.603s8-1.686 8-2.603V7.596c-.052.031-.112.059-.165.09a7.816 7.816 0 0 1-.745.386Z"/>
+          </svg>
+          <p className="read-the-docs text-left">
+            Connection Status: Online
+          </p>
+        </div>
+        <svg onClick={infoClicked} className="w-8 h-8 text-gray-800 dark:text-white mt-2 hover:text-gray-600 dark:hover:text-gray-400 cursor-pointer transition-transform transform hover:scale-110" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+          <path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z" clip-rule="evenodd"/>
+        </svg>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
