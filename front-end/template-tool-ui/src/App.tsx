f2bbc0a9d72b74a8c39046b8986f4e845e641a67
Callum Cheng <c3072423@hallam.shu.ac.uk>
2025-02-16 16:31:19 +0000

    positioned search and sending inputs

--- front-end/template-tool-ui/src/App.tsx
+++ front-end/template-tool-ui/src/App.tsx
@@ -15,15 +15,20 @@ function App() {
 
   return (
     <div className='p-4 pt-2 w-full h-full flex flex-col'>
-      <div className='flex grow w-full'>
+      <div className='flex w-full pb-4'>
         <div className='flex w-full justify-between'>
           <h1 className='text-left'>Template Tool</h1>
             <svg onClick={profileClicked} className="w-12 h-12 text-gray-800 dark:text-white mt-2 hover:text-gray-600 dark:hover:text-gray-400 cursor-pointer transition-transform transform hover:scale-110" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
-            <path fillRule="evenodd" d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z" clipRule="evenodd"/>
+              <path fillRule="evenodd" d="M12 20a7.966 7.966 0 0 1-5.002-1.756l.002.001v-.683c0-1.794 1.492-3.25 3.333-3.25h3.334c1.84 0 3.333 1.456 3.333 3.25v.683A7.966 7.966 0 0 1 12 20ZM2 12C2 6.477 6.477 2 12 2s10 4.477 10 10c0 5.5-4.44 9.963-9.932 10h-.138C6.438 21.962 2 17.5 2 12Zm10-5c-1.84 0-3.333 1.455-3.333 3.25S10.159 13.5 12 13.5c1.84 0 3.333-1.455 3.333-3.25S13.841 7 12 7Z" clipRule="evenodd"/>
             </svg>
         </div>
       </div>
-      <Search />
+      <div className='flex items-center w-full'>
+        <Search />
+      </div>
+      <div className='flex-grow flex items-center w-full'>
+        {/* Empty space that will grow */}
+      </div>
       <div className='flex justify-between items-center w-full'>
         <div className='flex items-center'>
           <svg className="w-8 h-8 text-green-400 pr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
--- front-end/template-tool-ui/src/pages/Search.tsx
+++ front-end/template-tool-ui/src/pages/Search.tsx
@@ -4,27 +4,41 @@ import { useNotification } from "../context/notification/useNotification";
 function Search() {
   const { addNotification } = useNotification();
   const [searchText, setSearchText] = useState('');
+  const [searchTeamFilter, setSearchTeamFilter] = useState('All Teams');
+  const [searchIncludeViewOnly, setSearchIncludeViewOnly] = useState(false);
 
   const searchClicked = () => {
-    addNotification(`Searching for: ${searchText}`);
+    addNotification(`Searching for: ${searchText}, Team: ${searchTeamFilter}, Include View Only: ${searchIncludeViewOnly}`);
+  };
+
+  const selectTeamFilterChanged =(selectedTeam: string) => {
+    setSearchTeamFilter(selectedTeam);
+  }
+
+  const checkboxViewOnlyClicked = () => {
+    setSearchIncludeViewOnly(!searchIncludeViewOnly);
   };
 
   return (
-    <div className="p-4 w-3/4">
-      <div className="flex justify-between mb-4 items-center">
-        <div>
-          <label className="mr-2">Dropdown:</label>
-          <select className="border rounded p-2">
-            <option value="option1">Option 1</option>
-            <option value="option2">Option 2</option>
-            <option value="option3">Option 3</option>
+    <div className="p-4 w-3/4 mx-auto">
+      <div className="flex justify-between mb-4 items-center space-x-8">
+        <div className="flex-1 flex items-center">
+          <label className="mr-2">Filter by Team:</label>
+          <select className="border rounded p-2"
+            value={searchTeamFilter}
+            onChange={(e) => selectTeamFilterChanged(e.target.value)}>
+            <option value="All Teams">All Teams</option>
+            <option value="Team 1">Team 1</option>
+            <option value="Team 2">Team 2</option>
           </select>
         </div>
         <div className="flex items-center">
-          <div className="mb-1">
-            <label className="mr-2" htmlFor="check">Checkbox:</label>
-          </div>
-          <input type="checkbox" id="check" className="w-4 h-4" />
+            <div className="mb-1 text-right pr-1">
+            <label className="mr-2" htmlFor="check">Include View-only templates</label>
+            </div>
+          <input type="checkbox" id="check" className="w-4 h-4" 
+            onClick={checkboxViewOnlyClicked}
+            checked={searchIncludeViewOnly}/>
         </div>
       </div>
       <div className="flex mb-4">
@@ -35,9 +49,10 @@ function Search() {
           value={searchText}
           onChange={(e) => setSearchText(e.target.value)}
         />
-        <button className="bg-blue-500 text-white p-2 rounded" onClick={searchClicked}>Search</button>
+        <button className="bg-blue-500 text-white p-2 pl-4 pr-4 rounded" onClick={searchClicked}>Search</button>
       </div>
       <hr />
+      <h2 className="p-4">Results</h2>
     </div>
   );
 }
